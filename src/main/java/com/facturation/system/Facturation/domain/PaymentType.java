eb2393146a3d4a45750bd888e54de6daee7cebfd
Jonnathan <91436348+Jonnathan1288@users.noreply.github.com>
2024-07-29 21:33:08 -0500

    feat: add lombok into class

--- src/main/java/com/facturation/system/Facturation/domain/PaymentType.java
+++ src/main/java/com/facturation/system/Facturation/domain/PaymentType.java
@@ -1,8 +1,23 @@
 package com.facturation.system.Facturation.domain;
 
-public class PaymentType {
-    
-    private Integer in;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name = "payment_types")
+public class PaymentType implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "description")
     private String description;
 }
