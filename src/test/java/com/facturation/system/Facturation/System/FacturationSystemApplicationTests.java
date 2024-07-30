9dc66284a1024ccf7a37e226acd6a09e171a5403
javier <92133005+javier-000@users.noreply.github.com>
2024-07-29 21:00:50 -0500

    add

---
+++ src/test/java/com/facturation/system/Facturation/System/FacturationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facturation.system.Facturation.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
