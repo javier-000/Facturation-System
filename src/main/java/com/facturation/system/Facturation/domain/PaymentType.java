f5042e32422cccf8e52aa070a037858524a5d3e3
Jonnathan <91436348+Jonnathan1288@users.noreply.github.com>
2024-07-29 21:26:12 -0500

    feat: add class

--- src/main/java/com/facturation/system/Facturation/domain/PaymentType.java
+++ src/main/java/com/facturation/system/Facturation/domain/PaymentType.java
@@ -1,28 +1,4 @@
-package com.system.agb.SystemAnimalCoupB.model;
+package com.facturation.system.Facturation.domain;
 
-
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-
-import java.io.Serializable;
-import java.time.LocalDateTime;
-import java.util.Date;
-
-@Entity
-@Getter
-@Setter
-@Table(name="payment_types")
-public class Solicitud implements Serializable {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id")
-    private Integer id;
-
-    @Column(name="name")
-    private String name;
-
-    @Column(name="description")
-    private String description;
-
+public class PaymentType {
 }
