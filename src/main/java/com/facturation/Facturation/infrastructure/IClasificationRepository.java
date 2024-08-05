cf4330b9ab2ad9476186fe98157f42255d92aa80
Jonnathan <91436348+Jonnathan1288@users.noreply.github.com>
2024-08-05 09:47:23 -0500

    feat: add

--- src/main/java/com/facturation/Facturation/infrastructure/IClasificationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Facturation.Facturation.infrastructure;
-
-import com.Facturation.Facturation.infrastructure.Generic.IGenericRepository;
-import com.Facturation.Facturation.model.Clasification;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IClasificationRepository extends IGenericRepository<Clasification, Integer> {
-}
